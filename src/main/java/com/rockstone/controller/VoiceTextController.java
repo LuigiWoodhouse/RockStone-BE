a8c10deda107642d60abb5a1e855d94e8b2bbd4d
LW611273 <luigi.woodhouse@digicelgroup.com>
2023-12-12 19:44:22 -0500

    added project structure,dependencies

---
+++ src/main/java/com/rockstone/controller/VoiceTextController.java
@@ -0,0 +1,10 @@
+package com.rockstone.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/voice/text")
+public class VoiceTextController {
+}
