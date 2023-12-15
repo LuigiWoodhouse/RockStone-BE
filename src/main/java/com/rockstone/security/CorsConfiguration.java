68619ebb7786bda3adf1b31768a97251b0eaae93
LW611273 <luigi.woodhouse@digicelgroup.com>
2023-12-14 21:12:11 -0500

    update configs for master

--- src/main/java/com/rockstone/security/CorsConfiguration.java
+++ src/main/java/com/rockstone/security/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://rockstone-hackathon.azurewebsites.net")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
